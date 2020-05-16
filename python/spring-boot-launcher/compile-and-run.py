88d00a462f60423cd615e44ab79de91d89a215e4
lxdi <alexfordeal@gmail.com>
2020-05-16 14:00:12 +0300

    udpated scripts for spring boot

--- python/spring-boot-launcher/compile-and-run.py
+++ python/spring-boot-launcher/compile-and-run.py
@@ -3,6 +3,17 @@ import os
 
 version = "1.0"
 print("Script version: %s" % version)
+
+pidFileName = 'pid.file'
+if os.path.isfile(pidFileName):
+	print('--------------------- Shutting down previous --------------------------------')
+	for line in open(pidFileName):
+		pid = line
+	shutdownCmd = 'kill %s' % pid
+	subprocess.Popen(shutdownCmd, shell=True)
+	print('Shutdown: done')
+
+
 print("-----------------------------------------Compilation----------------------------------")
 initDir = os.getcwd()
 os.chdir('..')
@@ -12,9 +23,9 @@ for line in process.stdout:
 process.wait()
 os.chdir(initDir)
 
-debugPort = '5006'
+debugPort = '5007'
 debugOption = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=%s' % debugPort
-shutdownOption = '& echo $! > ./pid.file'
+shutdownOption = '& echo $! > ./%s' % pidFileName
 warPath = os.path.join(os.getcwd(), '..', 'backend', 'build', 'libs', 'ROOT.jar')
 processjava = subprocess.Popen('java %s -jar %s %s' % (debugOption, warPath, shutdownOption), shell=True)
 print('Started wiht pid ' + str(processjava.pid))
--- python/spring-boot-launcher/shutdown.py
+++ python/spring-boot-launcher/shutdown.py
@@ -1,6 +1,13 @@
 import subprocess
 import os
 
-shutdownCmd = 'kill $(cat ./pid.file)'
+pidFileName = 'pid.file'
+file = open(pidFileName)
+for line in file:
+    pid = line
+
+shutdownCmd = 'kill %s' % pid
 processjava = subprocess.Popen(shutdownCmd, shell=True)
 print('Shutdown: done')
+
+os.remove(pidFileName)
