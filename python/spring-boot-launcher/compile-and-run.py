d61cf0bb6ecdb85a931eb93867fc46a3b7eb4890
lxdi <alexfordeal@gmail.com>
2021-10-14 14:46:02 +0300

    compile-and-run.py

--- python/spring-boot-launcher/compile-and-run.py
+++ python/spring-boot-launcher/compile-and-run.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-version = "1.1"
+version = "1.2"
 print("Script version: %s" % version)
 
 pidFileName = 'pid.file'
@@ -20,14 +20,21 @@ print("-----------------------------------------Compilation---------------------
 initDir = os.getcwd()
 os.chdir('..')
 process = subprocess.Popen(['./gradlew clean bootJar'], shell=True, stdout = subprocess.PIPE)
+
 for line in process.stdout:
 	print(line.decode('utf-8').replace('\n', ''))
+
 process.wait()
 os.chdir(initDir)
+
+print("-----------------------------------------Starting----------------------------------")
 
 debugPort = '5005'
 debugOption = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=%s' % debugPort
 shutdownOption = '& echo $! > ./%s' % pidFileName
-warPath = os.path.join(os.getcwd(), '..', 'backend', 'build', 'libs', 'ROOT.jar')
-processjava = subprocess.Popen('java %s -jar %s %s' % (debugOption, warPath, shutdownOption), shell=True)
-print('Started wiht pid ' + str(processjava.pid))
+
+jarFolder = os.path.join(os.getcwd(), '..', 'backend', 'build', 'libs')
+jarPath = os.path.join(jarFolder, os.listdir(jarFolder)[0])
+
+processJava = subprocess.Popen('java %s -jar %s %s' % (debugOption, jarPath, shutdownOption), shell=True)
+print('Started with pid ' + str(processJava.pid))
